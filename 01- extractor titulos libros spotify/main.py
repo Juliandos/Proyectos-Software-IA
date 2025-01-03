5cd06f36e544e3310c55a55ea96a0062505cf29a
Juliandos <95juliandos@gmail.com>
2025-01-03 11:22:06 -0600

    primera demo de acceso a la api

--- 01- extractor titulos libros spotify/main.py
+++ 01- extractor titulos libros spotify/main.py
@@ -1,3 +1,15 @@
-print('hola')
-print('hola')
-print('hola')
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Configura tus credenciales
+client_id = "52b59a52c48b4780b87d8b4e61932f73"
+client_secret = "3b7f1e46084b4338a68601ff3c654503"
+
+# Autenticación
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+# Ejemplo: Buscar una canción
+result = sp.search(q='Imagine Dragons', type='track', limit=5)
+for idx, track in enumerate(result['tracks']['items']):
+    print(f"{idx + 1}. {track['name']} - {track['artists'][0]['name']}")
