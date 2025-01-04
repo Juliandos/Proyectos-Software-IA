856f1edfca79675ac280121b0bd0d82ad63c9daf
Juliandos <95juliandos@gmail.com>
2025-01-03 21:15:42 -0600

    creación .env y uso de dot-env

--- 01- extractor titulos libros spotify/main.py
+++ 01- extractor titulos libros spotify/main.py
@@ -1,15 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Configura tus credenciales
-client_id = "52b59a52c48b4780b87d8b4e61932f73"
-client_secret = "3b7f1e46084b4338a68601ff3c654503"
+client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 # Autenticación
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 # Ejemplo: Buscar una canción
-result = sp.search(q='Imagine Dragons', type='track', limit=5)
+result = sp.search(q='canserbero', type='track', limit=5)
 for idx, track in enumerate(result['tracks']['items']):
     print(f"{idx + 1}. {track['name']} - {track['artists'][0]['name']}")
